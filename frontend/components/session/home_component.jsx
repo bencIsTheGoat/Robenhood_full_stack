df74a00d16cd6f06bac9dfac8abc57a7d14769a4
Ben Cutler <bcutler94@gmail.com>
2019-03-27 18:19:19 -0400

    Update splash page

--- frontend/components/session/home_component.jsx
+++ frontend/components/session/home_component.jsx
@@ -42,8 +42,8 @@ class Home extends React.Component {
             </div>
             <div className='body-text-img'>
                 <div className='login-text'>
-                    <h1>Invest Commission-Free</h1>
-                    <p>Invest in stocks, ETFs, options,<br/> and cryptocurrencies, all commission-free,<br/> right from your phone or desktop.</p> 
+                    <h1>Invest <br/>Commission-Free</h1>
+                    <p>Invest in stocks, ETFs, options, <br/>and cryptocurrencies, all commission-free,<br/>right from your phone or desktop.</p> 
                 </div>
                 <div className='img'>
                     <img src='/assets/phone_images-7840ce35b41e034c6cd343a97aab2ede84882c31f67a44da6bda9d325b58ce9c.png' alt="two phones displaying stock info"/>
